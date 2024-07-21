dff727a8a8a51da89cd3d54fd9fd842e4db207c2
Wiris Wernek <wiriswernek@gmail.com>
2024-07-21 02:00:40 -0300

    feat: add view de login

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -1,5 +1,14 @@
-import { Routes } from "@angular/router";
+import { Routes } from '@angular/router'
 
 export const AUTH_ROUTES: Routes = [
-	
+   {
+      path: 'login',
+      loadComponent: () => import('./views/login/login.component').then((c) => c.LoginComponent),
+   },
+
+   {
+      path: '**',
+      pathMatch: 'full',
+      redirectTo: 'login',
+   },
 ]

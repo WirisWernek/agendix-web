86fb5e959a6fc5360720fe9f783ebf5890a034ca
Wiris Wernek <wiriswernek@gmail.com>
2024-07-21 20:38:07 -0300

    feat: add home administrador

--- src/app/admin/admin.routes.ts
+++ src/app/admin/admin.routes.ts
@@ -1,5 +1,13 @@
-import { Routes } from "@angular/router";
+import { Routes } from '@angular/router'
 
 export const ADMIN_ROUTES: Routes = [
-	
+   {
+      path: 'home',
+      loadComponent: () => import('./views/home/home.component').then((c) => c.HomeComponent),
+   },
+   {
+      path: '**',
+      pathMatch: 'full',
+      redirectTo: 'home',
+   },
 ]

7e1f01b492288c30e8579de06ed0f56add90e06e
Wiris Wernek <wiriswernek@gmail.com>
2024-07-21 20:38:51 -0300

    feat: add home prestador

--- src/app/prestador/prestador.routes.ts
+++ src/app/prestador/prestador.routes.ts
@@ -1,5 +1,13 @@
 import { Routes } from "@angular/router";
 
 export const PRESTADOR_ROUTES: Routes = [
-	
+	{
+		path: 'home',
+		loadComponent: () => import('./views/home/home.component').then((c) => c.HomeComponent),
+	 },
+	 {
+		path: '**',
+		pathMatch: 'full',
+		redirectTo: 'home',
+	 },
 ]

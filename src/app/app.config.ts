f5dbf9619c221766cb862287ac79a51521d9526e
Wiris Wernek <wiriswernek@gmail.com>
2024-07-21 20:41:35 -0300

    feat: adiciona angular-jwt, configuração padrão dele e provider do httpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http'
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core'
+import { provideRouter } from '@angular/router'
+import { JwtModule } from '@auth0/angular-jwt'
+import { routes } from './app.routes'
+export function tokenGetter() {
+   return localStorage.getItem('token')
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
-};
+   providers: [
+      provideZoneChangeDetection({ eventCoalescing: true }),
+      provideRouter(routes),
+	  provideHttpClient(),
+      importProvidersFrom(
+         JwtModule.forRoot({
+            config: {
+               tokenGetter: tokenGetter,
+               allowedDomains: ['localhost:8082', '192.168.18.19:8082'],
+            },
+         })
+      ),
+   ],
+}

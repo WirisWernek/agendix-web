06677d369016672bcc7270855e2a6f375330066d
Wiris Wernek <wiriswernek@gmail.com>
2024-07-20 22:55:23 -0300

    feat: adiciona arquivos de roteamento de cada modulo e remove código html não usado do componente principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes').then((r) => r.AUTH_ROUTES),
+  },
+  {
+    path: 'cliente',
+    loadChildren: () =>
+      import('./cliente/cliente.routes').then((r) => r.CLIENTE_ROUTES),
+  },
+  {
+    path: 'prestador',
+    loadChildren: () =>
+      import('./prestador/prestador.routes').then((r) => r.PRESTADOR_ROUTES),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.routes').then((r) => r.ADMIN_ROUTES),
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'auth',
+  },
+];

017fbaae4c0274fd2d4d90b2fe7158b446c52100
Wiris Wernek <wiriswernek@gmail.com>
2024-07-21 20:38:30 -0300

    feat: add home cliente

--- src/app/cliente/cliente.routes.ts
+++ src/app/cliente/cliente.routes.ts
@@ -1,5 +1,13 @@
 import { Routes } from "@angular/router";
 
 export const CLIENTE_ROUTES: Routes = [
-	
+	{
+		path: 'home',
+		loadComponent: () => import('./views/home/home.component').then((c) => c.HomeComponent),
+	 },
+	 {
+		path: '**',
+		pathMatch: 'full',
+		redirectTo: 'home',
+	 },
 ]

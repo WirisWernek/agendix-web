153c5a1c85b82b0ac613da2aa7a241d1d5b6ad95
Wiris Wernek <wiriswernek@gmail.com>
2024-08-26 22:13:06 -0300

    feat: add communication view for managing customer communications

---
+++ src/app/prestador/views/comunicacao/comunicacao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+   selector: 'app-comunicacao',
+   standalone: true,
+   imports: [],
+   templateUrl: './comunicacao.component.html',
+   styleUrl: './comunicacao.component.scss',
+})
+export class ComunicacaoComponent {
+	novoEnvio() {}
+}

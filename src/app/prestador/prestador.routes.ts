1caa2a6400f821f9c01995e160547d5b653a3ec3
Wiris Wernek <wiriswernek@gmail.com>
2024-08-26 22:14:08 -0300

    feat: add new routes for managing customers clients, links, and communications

--- src/app/prestador/prestador.routes.ts
+++ src/app/prestador/prestador.routes.ts
@@ -1,13 +1,25 @@
-import { Routes } from "@angular/router";
+import { Routes } from '@angular/router'
 
 export const PRESTADOR_ROUTES: Routes = [
-	{
-		path: 'home',
-		loadComponent: () => import('./views/home/home.component').then((c) => c.HomeComponent),
-	 },
-	 {
-		path: '**',
-		pathMatch: 'full',
-		redirectTo: 'home',
-	 },
+   {
+      path: 'home',
+      loadComponent: () => import('./views/home/home.component').then((c) => c.HomeComponent),
+   },
+   {
+      path: 'links',
+      loadComponent: () => import('./views/links/links.component').then((c) => c.LinksComponent),
+   },
+   {
+      path: 'comunicacao',
+      loadComponent: () => import('./views/comunicacao/comunicacao.component').then((c) => c.ComunicacaoComponent),
+   },
+   {
+      path: 'clientes',
+      loadComponent: () => import('./views/clientes/clientes.component').then((c) => c.ClientesComponent),
+   },
+   {
+      path: '**',
+      pathMatch: 'full',
+      redirectTo: 'home',
+   },
 ]
